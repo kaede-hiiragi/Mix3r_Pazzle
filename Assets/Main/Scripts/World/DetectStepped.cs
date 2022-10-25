37f8139152e9344df8fac007cef6e5353f9ead88
Suigomi <49914693+suigomi@users.noreply.github.com>
2022-10-25 19:28:43 +0900

    床がプレイヤーの位置を教えてくれるように
    x軸またはz軸が一致したら明るくなる

--- Assets/Main/Scripts/World/DetectStepped.cs
+++ Assets/Main/Scripts/World/DetectStepped.cs
@@ -5,19 +5,34 @@ using UnityEngine;
 public class DetectStepped : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Material _initialMaterial;
     public Material _steppedMaterial;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        this.GetComponent<Renderer>().material = _steppedMaterial;
-    }
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     this.GetComponent<Renderer>().material = _steppedMaterial;
+    // }
+
+    // private void OnTriggerExit(Collider other)
+    // {
+    //     this.GetComponent<Renderer>().material = _initialMaterial;
+    // }
+
     void Start()
     {
-        
+        _initialMaterial = this.GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if((GameManager.instance._playerPosition.x == this.transform.position.x || GameManager.instance._playerPosition.z == this.transform.position.z))
+        {
+            this.GetComponent<Renderer>().material = _steppedMaterial;
+        }
+        else
+        {
+            this.GetComponent<Renderer>().material = _initialMaterial;
+        }
     }
 }
